cd14da1bcfed6a99fc4d813c4a55ec233dffb278
kanchan tonape <tonapedkanchan@gmail.com>
2021-04-23 10:18:52 +0000

    testing new build

---
+++ webserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum install httpd 
+echo "<h1>This is webpage has been built with help of jenkins</h1>" > /var/www/html/index.html
+systemctl start httpd
+systemctl status httpd
+sleep 10 
